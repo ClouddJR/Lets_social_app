05383ad5a97a3f820bc9edb0316e7425ed7aacfb
Arkadiusz Chmura <arekchmura@gmail.com>
2018-09-08 22:08:47 +0200

    event model update

--- app/src/main/java/com/lets/app/model/Event.kt
+++ app/src/main/java/com/lets/app/model/Event.kt
@@ -2,22 +2,22 @@ package com.lets.app.model
 
 import com.google.firebase.Timestamp
 import com.google.firebase.firestore.GeoPoint
+import java.util.*
 
 data class Event(
-        val owner: String,
-        val title: String,
-        val description: String,
-        val timestamp: Timestamp,
-        val location: GeoPoint,
-        val isPublic: Boolean,
-        val maxPeople: Int,
-        val ageFrom: Int,
-        val ageTo: Int,
-        val sex: Int,
-        val type: Int,
-        val category: Int,
-        val isBlocked: Boolean,
-        val joined: List<User>,
-        val request: List<Request>
-
+        val owner: String = "",
+        val title: String = "",
+        val description: String = "",
+        val timestamp: Timestamp = Timestamp(Date()),
+        val location: GeoPoint = GeoPoint(0.0, 0.0),
+        val isPublic: Boolean = true,
+        val maxPeople: Int = 0,
+        val ageFrom: Int = 0,
+        val ageTo: Int = 0,
+        val sex: Int = 0,
+        val type: Int = 0,
+        val category: Int = 0,
+        val isBlocked: Boolean = false,
+        val joined: List<User> = listOf(),
+        val request: List<Request> = listOf()
 )
