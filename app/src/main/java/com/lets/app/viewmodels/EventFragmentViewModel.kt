6b88e6a135a6d1a0d0ce4e6c30e243caa64a6e19
Arkadiusz Chmura <arekchmura@gmail.com>
2018-10-07 13:39:46 +0200

    getting user info alongside event details for EventFragmentViewModel

--- app/src/main/java/com/lets/app/viewmodels/EventFragmentViewModel.kt
+++ app/src/main/java/com/lets/app/viewmodels/EventFragmentViewModel.kt
@@ -1,34 +1,42 @@
 package com.lets.app.viewmodels
 
-import android.content.Context
-import android.location.Address
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.firestore.GeoPoint
-import com.lets.app.repositories.GeocodingRepository
+import com.lets.app.EventsRepository
+import com.lets.app.model.Event
+import com.lets.app.model.User
+import com.lets.app.repositories.UserRepository
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 
 class EventFragmentViewModel : ViewModel() {
 
-    val addressOfLocation = MutableLiveData<List<Address>>()
+    private val eventsRepository = EventsRepository()
+    private val userRepository = UserRepository()
 
-    private val geocodingRepository = GeocodingRepository()
     private lateinit var disposable: Disposable
 
-    fun init(location: GeoPoint, context: Context) {
-        disposable = geocodingRepository.getAddressOfLocation(context, location)
+    val event = MutableLiveData<Event>()
+    val user = MutableLiveData<User>()
+
+    fun init(eventId: String, eventLocation: GeoPoint) {
+        disposable = eventsRepository.getEvent(eventId, eventLocation)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
+                .flatMap {
+                    event.value = it
+                    userRepository.getUserFromId(it.owner)
+                }
                 .subscribe {
-                    addressOfLocation.value = it
+                    user.value = it
                 }
     }
 
     override fun onCleared() {
         super.onCleared()
-        if(!disposable.isDisposed) {
+        if (!disposable.isDisposed) {
             disposable.dispose()
         }
     }
