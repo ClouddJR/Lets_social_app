2a99025fb706edd88c6b4df902f5dada41010f1f
Arkadiusz Chmura <arekchmura@gmail.com>
2018-12-08 18:14:18 +0100

    Dagger2 component injected in Application class

--- app/src/main/java/com/lets/app/LetsApplication.kt
+++ app/src/main/java/com/lets/app/LetsApplication.kt
@@ -4,12 +4,22 @@ import android.app.Application
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreSettings
+import com.lets.app.di.ApplicationComponent
+import com.lets.app.di.ApplicationModule
+import com.lets.app.di.DaggerApplicationComponent
 import com.squareup.leakcanary.LeakCanary
 
 class LetsApplication : Application() {
 
+    val component: ApplicationComponent by lazy {
+        DaggerApplicationComponent.builder()
+                .applicationModule(ApplicationModule(this))
+                .build()
+    }
+
     override fun onCreate() {
         super.onCreate()
+        component.inject(this)
         if (LeakCanary.isInAnalyzerProcess(this)) {
             return
         }
